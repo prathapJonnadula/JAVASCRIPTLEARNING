51fd0151a72b0a895d56f5613d0658789cf229be
prathapJonnadula <prathapjonnadula@gmail.com>
2019-07-27 18:11:00 -0700

    uploadjs

---
+++ js/app.js
@@ -0,0 +1,15 @@
+console.log('sayhello');
+console.log(123);
+console.log(true);
+var greeting= 'hello';
+console.log(greeting);
+console.log([1,2,3,4]);
+console.log({a:1,b:2});
+console.table({a:1,b:2});
+console.error("this is error");
+console.clear();
+console.warn("this is warning");
+console.time('hello');
+  console.log("hello Woorld");
+  console.timeEnd("hello");
+alert('hello');
---
+++ js/app_var.js
@@ -0,0 +1,41 @@
+var name = 'Jhon Deoe';
+console.log(name);
+name = "Steve smith";
+console.log(name);
+ 
+var greeting 
+console.log(greeting);
+greeting = "hello";
+console.log("greeting");
+
+//letters, nummbers,_, $
+//can not start with numbers
+
+//Multi world vars
+var firstName = "jon";
+var middle_Name = "Sara";
+var FirstName ='Tom';
+var firstName;
+
+//let
+// let name = 'jhon Doe';
+console.log(name);
+
+//const
+
+// const name = 'jhon';
+// console.log(name);
+
+// const greeting;
+
+const person = {
+  name:'jhon',
+  age :30
+}
+
+person.name = 'sara'
+person.age = 32;
+
+const number = [1,2,3,4,5];
+number.push(6);
+console.log(number);
